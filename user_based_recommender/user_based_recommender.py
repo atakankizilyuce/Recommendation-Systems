2bc6a8c1986c9ddd2f7ba6bbac0a61717e725fa7
Atakan Kızılyüce <99099880+atakankizilyuce@users.noreply.github.com>
2023-01-23 22:08:39 +0300

    Update user_based_recommender.py

--- user_based_recommender/user_based_recommender.py
+++ user_based_recommender/user_based_recommender.py
@@ -2,12 +2,12 @@
 # User-Based Collaborative Filtering
 #############################################
 
-# Adım 1: Veri Setinin Hazırlanması
-# Adım 2: Öneri Yapılacak Kullanıcının İzlediği Filmlerin Belirlenmesi
-# Adım 3: Aynı Filmleri İzleyen Diğer Kullanıcıların Verisine ve Id'lerine Erişmek
-# Adım 4: Öneri Yapılacak Kullanıcı ile En Benzer Davranışlı Kullanıcıların Belirlenmesi
-# Adım 5: Weighted Average Recommendation Score'un Hesaplanması
-# Adım 6: Çalışmanın Fonksiyonlaştırılması
+#      1. Veri Setinin Hazırlanması
+#      2. Öneri Yapılacak Kullanıcının İzlediği Filmlerin Belirlenmesi
+#      3. Aynı Filmleri İzleyen Diğer Kullanıcıların Verisine ve Id'lerine Erişmek
+#      4. Öneri Yapılacak Kullanıcı ile En Benzer Davranışlı Kullanıcıların Belirlenmesi
+#      5. Weighted Average Recommendation Score'un Hesaplanması
+#      6. Çalışmanın Fonksiyonlaştırılması
 
 #############################################
 # Adım 1: Veri Setinin Hazırlanması
